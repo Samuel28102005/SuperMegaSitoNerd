87c247090cc05b9fe6c5514b8c8a47ea1bc127ef
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2023-05-08 12:51:27 +0000

    punto 2

--- sito-nerd/src/app/black-dragon/black-dragon.component.ts
+++ sito-nerd/src/app/black-dragon/black-dragon.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
-@Component({
+import { Root } from '../app.component';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';@Component({
   selector: 'app-black-dragon',
   templateUrl: './black-dragon.component.html',
   styleUrls: ['./black-dragon.component.css']
 })
 export class BlackDragonComponent {
+  title = 'nello';
+  data! : Root;
+  pic = String;
+  loading!: boolean;
+  o = new Observable<Object>;
+  constructor(public http: HttpClient) {}
 
+
+   makeRequest(): void {
+     console.log("here");
+     this.loading = true;
+     this.o = this.http.get('https://api.scryfall.com/cards/named?fuzzy=aust+com');
+     this.o.subscribe(this.getData);
+   }
+   getData = (d : any) =>
+   {
+     this.data = d["data"];
+     this.loading = false;
+     this.pic = String;
+   }
 }
---
+++ sito-nerd/src/app/black-dragon/black-dragon.model.ts
@@ -0,0 +1,6 @@
+export interface Welcome7 {
+    object:  string;
+    code:    string;
+    status:  number;
+    details: string;
+}
