9e504ccfafeb98113a2bf23366265beaf46cc77f
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2023-05-11 11:58:36 +0000

    versione finita generic

--- sito-nerd/src/app/app-routing.module.ts
+++ sito-nerd/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GenericComponent } from './generic/generic.component';
 const routes: Routes = [
+  {path: 'carta/:id', component: GenericComponent}
 ];
 
 @NgModule({
--- sito-nerd/src/app/generic/generic.component.ts
+++ sito-nerd/src/app/generic/generic.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { Root } from '../app.component';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Root } from '../white-monster/white-monster.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-generic',
@@ -10,42 +10,23 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./generic.component.css']
 })
 export class GenericComponent {
-  title = 'vesuvio esplodi';
-  data! : Root;
-  pic = String;
-  loading!: boolean;
-  o !: Observable<Root>;
-  ola: string | undefined;
-  constructor(public http: HttpClient,private route: ActivatedRoute) {
-
-      //Gestisce i cambi di route con l'observable paramMap
-      this.route.paramMap.subscribe(this.getRouterParam);
-    }
-   
-    //Ogni volta che viene invocata la route tracks/:id, l'observable paramMap richiama questo metodo
-    getRouterParam = (params: ParamMap) =>
-    {
-      let uri_param = params.get('id'); //Ottengo l'id dalla ParamMap
-      console.log (uri_param); //Stampo su console
-      //this.service.getTrack()
-      if (uri_param == 'black-dragon') this.ola = 'https://api.scryfall.com/cards/named?fuzzy=aust+com'  ;
-      if (uri_param == 'white-monster') this.ola = 'https://api.scryfall.com/cards/named?fuzzy=white-shield-crusader';
-      if (uri_param == 'young-red-dragon') this.ola = 'https://api.scryfall.com/cards/named?fuzzy=a-young-red-dragon-a-bathe-in-gold';
-      if (uri_param == 'green-slime') this.ola = 'https://api.scryfall.com/cards/named?fuzzy=green-slime';
-      if (uri_param == 'purple-crab') this.ola = 'https://api.scryfall.com/cards/named?fuzzy=purple-crab';
-
-
-      
-    }
-  l(): void {
-    console.log("here");
-     this.loading = true;
-     this.o = this.http.get<Root>('https://api.scryfall.com/cards/named?fuzzy=green-slime');
-     this.o.subscribe(this.getData);
+  carta!: String | null
+  data: Root | undefined;
+  loading: boolean | undefined;
+  obs_carta : Observable<Root> | undefined;
+  constructor(private route: ActivatedRoute, public http: HttpClient) {
+    this.route.paramMap.subscribe(this.getRouterParam);
   }
-   getData = (d : Root) =>
+  getRouterParam = (params: ParamMap) =>
+  {
+    this.carta = params.get('id'); //Ottengo l'id dalla ParamMap
+    console.log (this.carta); 
+    this.obs_carta = this.http.get<Root>('https://api.scryfall.com/cards/named?fuzzy=' + this.carta);
+    this.obs_carta.subscribe(this.getData)
+  }
+  getData = (d : Root) =>
    {
-     this.loading = (false);
      this.data = (d);
+     this.loading = false;
    }
-  }
+}
